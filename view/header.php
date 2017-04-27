dfe0d8580354515e5d7c353276f0118003196ee7
Mahana <mahana.delacour@gmail.com>
2017-04-27 17:22:17 +0200

    dernier commit

--- view/header.php
+++ view/header.php
@@ -1,12 +1,12 @@
-<header class="header row">
-  <a  class="col-xs-8 col-lg-6" href="../controller/index.php"><h1>GREEN</h1></a>
-  <a class="col-xs-2 col-lg-2" href="../controller/profilCtrl.php"><h3>Bonjour
+<header class="header row ">
+  <a  class="green col-xs-8 col-lg-6 col" href="../controller/index.php"><h1>GREEN</h1></a>
+  <a class="blue col-xs-8 col-lg-2" href="../controller/profilCtrl.php"><p>Bonjour
     <?php
     echo $thisuser;
-    ?> !</h3></a>
+    ?> !</p></a>
 
-  <a href="../controller/n_art_controller.php"><button class="col-xs-1 col-lg-2" type="button">+</button></a>
-  <a href="../controller/sign_up_controller.php"><p class="col-xs-1 col-lg-2">Connexion</p></a>
+  <a href="../controller/n_art_controller.php"><button class="red col-xs-4 col-lg-2 col" type="button">+</button></a>
+  <a href="../controller/sign_up_controller.php"><p class="pink col-xs-12 col-lg-2 col">Connexion</p></a>
   <?php if ($thisuser) { ?>
     <a href="../controller/logoutCtrl.php">Log out</a>
   <?php } ?>
